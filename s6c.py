530126a8aca6bc6d0eeb6c00162c3c3a0d93f775
Kai Marshland <kaimarshland@gmail.com>
2018-10-20 11:01:30 -0700

    Log raw data to file 2

--- s6c.py
+++ s6c.py
@@ -53,13 +53,13 @@ def run_connection():
 
 
 def listen():
-    outfile = open("data.bin", "ab")
+    outfile = open("data.txt", "a")
 
     while True:
         frames = handler.poll()
         if frames:
             for f in frames:
-                outfile.write(f.payload)
+                outfile.write(''.join(f.payload) + '\n')
                 out = struct.unpack("iii", f.payload[1:13])
                 print(out)
                 data = {"id":str(uuid.uuid4()), "mission": 53, "timestamp": int(time.time()*1000), "latitude": out[0]/1000000, "longitude": out[1]/1000000, "altitude": out[2]}
